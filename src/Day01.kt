362e5440d9d96c60a534af10f420745b3b4dffd2
SidhantRajora <sidhant.rajora@target.com>
2022-12-01 12:25:46 +0530

    feat(Day-1): the calories of the elves

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,37 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+
+    fun prepare(input:String): List<Long> {
+        return input.split("\n\n").map { it.split("\n") }.map {
+            it.sumOf { item -> item.toLong() }
+        }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Long>): Triple<Long, Long, Long> {
+        var (first, second, third) = arrayOf(Long.MIN_VALUE, Long.MIN_VALUE, Long.MIN_VALUE)
+
+        for(e in input){
+            if(e>first){
+                third = second
+                second = first
+                first = e
+            }else if (e > second) {
+                third = second
+                second = e
+            }else if (e > third) {
+                third = e
+            }
+        }
+
+        return Triple(first, second, third)
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    fun part2(triple: Triple<Long, Long, Long>): Long {
+        return triple.first+triple.second+triple.third
+    }
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val input = readAsString("Day01")
+    val processedInput = prepare(input)
+    val triple = part1(processedInput)
+    println(triple.first)
+    println(part2(triple))
 }
